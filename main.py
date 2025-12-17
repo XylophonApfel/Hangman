21926772e0e1caaacd0cd2aca93962dd6be65e89
XylophonApfel <lanzendoerfer.aron@gmail.com>
2025-12-17 12:24:11 +0100

    [Aron] Kommentare hinzugefügt

--- main.py
+++ main.py
@@ -5,19 +5,28 @@ from tui import *
 import os
 
 def main():
+    # Lesen und Speichern der Wörter datei in repo_txt
     liste_woerte = open_file()
+    # Wort per zufall auswählen in management
     wort = wort_erzeugen(liste_woerte)
+    # Leerzeichen entfernen
     wort = wort.strip()
+    # Eingabe Anzahl der Spieler in management
     anzahl_Spieler = menge_Spieler()
+    # Zuordnung der Namen in management
     Liste_Namen = eingabe_Spieler(anzahl_Spieler)
+
+    # Erste Anzeige zum Starten des Spieles in tui
     os.system("cls")
     laenge_wort(wort)
-    #print(wort)
+
+    # Notwendige Variabeln für die Darstellung
     Erraten = []
     buchstabe_fehler = []
     Richtig = ""
     zaehler = 0
 
+    # Beginn von Abfrag
     while zaehler < 6 and Richtig != "Richtig":
         for Name in Liste_Namen:
             print(f"\n{Name} ist Dran!\n")
